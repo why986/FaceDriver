4f97455fc78cf620b058dd37f3ca0373f617e9fd
why986 <986496569@qq.com>
2021-07-28 15:08:04 +0800

    add demo.y

---
+++ demo.py
@@ -0,0 +1,12 @@
+from audio2video.API import audio_to_landmarks
+from audio2video.utils import *
+
+file_name = '5_0000'
+audio_path = f'audio2video/data/{file_name}.wav'
+size = (255, 255)
+
+landmarks_dir = 'audio2video/data/face_npy'
+face_aver = get_average_face(landmarks_dir)
+
+path = audio_to_landmarks(audio_path, size, face_aver[0], model_name='multi_task_l2_1', model_dir='')
+print(path)
